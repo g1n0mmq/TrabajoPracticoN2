a985868549b4aa2dbf5f3a3007b0f127f498b96d
Murua Gino Miguel <gmggino10@gmail.com>
2025-05-22 21:36:19 -0300

    Primer commit del ejercicio 4

---
+++ Ejercicio_4/script.js
@@ -0,0 +1,16 @@
+
+        const boton = document.getElementById("calcular");
+
+        boton.addEventListener("click", calcularIMC);
+        function calcularIMC() {
+            var peso = parseFloat(document.getElementById("peso").value);
+            var altura = parseFloat(document.getElementById("altura").value);
+
+            if (peso > 0 && altura > 0) {
+                altura = altura / 100; 
+                var imc = peso / (altura * altura);
+                alert("Tu IMC es: " + imc.toFixed(2));
+            } else {
+                alert("Por favor, ingresa valores válidos para peso y altura.");
+            }
+        }
